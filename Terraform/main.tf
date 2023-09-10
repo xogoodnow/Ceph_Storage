4c3f410dfc7dd990c3aeda4a357072c80ee29e3b
xogoodnow <xo4ever9999@gmail.com>
2023-09-10 13:30:12 +0330

    Added private network module to the main terraform file :sparkles:

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,13 +6,22 @@ module "ssh_keys" {
 }
 
 
+module "Private_Network" {
+
+  source = "./modules/Private_Network"
+  hcloud_token = var.hcloud_token
+
+}
+
+
 module "servers" {
   source       = "./modules/servers"
   hcloud_token = var.hcloud_token
   image_name   = var.image_name
   server_type  = var.server_type
   location     = var.location
-  depends_on   = [module.ssh_keys]
+  depends_on   = [module.ssh_keys, module.Private_Network]
+
 }
 
 module "volume" {
