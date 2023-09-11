bf8b8e6664d1b4998a91e5ce438cbbd3681a7d2d
xogoodnow <xo4ever9999@gmail.com>
2023-09-11 17:05:56 +0330

    Fixed issued :bug:

--- Terraform/modules/Ceph/main.tf
+++ Terraform/modules/Ceph/main.tf
@@ -1,7 +1,7 @@
 
 resource "null_resource" "k8s" {
   provisioner "local-exec" {
-    command = "sleep 111  && PWD='../' ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.yaml ../playbook/setup.yaml --private-key sshkey/private_key.pem"
+    command = "sleep 111  && PWD='../' ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.yaml .../../../Ansible/playbooks/setup.yaml --private-key sshkey/private_key.pem"
   }
 }
 
