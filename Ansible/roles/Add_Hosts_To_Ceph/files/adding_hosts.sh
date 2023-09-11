d5efb89a134bd4088203b530708e6138bf4217ec
xogoodnow <xo4ever9999@gmail.com>
2023-09-11 23:14:59 +0330

    Fixed logical issues and passed the tests :white_check_mark:

--- Ansible/roles/Add_Hosts_To_Ceph/files/adding_hosts.sh
+++ Ansible/roles/Add_Hosts_To_Ceph/files/adding_hosts.sh
@@ -7,26 +7,29 @@ while IFS= read -r line; do
 
     # Check if there are at least two fields (hostname and IP address)
     if [ ${#fields[@]} -ge 2 ]; then
-        hostname="${fields[0]}"
-        ip="${fields[1]}"
+        hostname="${fields[1]}"  # Use the second field as the hostname
+        ip="${fields[0]}"        # Use the first field as the IP address
 
         # Check if the IP address is a valid IPv4 or IPv6 address
         if [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ || "$ip" =~ : ]]; then
-            # Check if the hostname contains "osd"
-            if [[ "$hostname" == *"osd"* ]]; then
-                labels="--labels=osd"
-            # Check if the hostname contains "rgw"
-            elif [[ "$hostname" == *"rgw"* ]]; then
-                labels="--labels=rgw"
-            # Check if the hostname contains "mon"
-            elif [[ "$hostname" == *"mon"* ]]; then
-                labels="--labels=_admin,mon"
-            else
-                labels=""
-            fi
+            # Exclude specific hostnames
+            if [[ "$hostname" != "ip6-allnodes" && "$hostname" != "ip6-allrouters" && "$hostname" != "localhost" ]]; then
+                # Check if the hostname contains "osd"
+                if [[ "$hostname" == *"osd"* ]]; then
+                    labels="--labels=osd"
+                # Check if the hostname contains "rgw"
+                elif [[ "$hostname" == *"rgw"* ]]; then
+                    labels="--labels=rgw"
+                # Check if the hostname contains "mon"
+                elif [[ "$hostname" == *"mon"* ]]; then
+                    labels="--labels=_admin,mon"
+                else
+                    labels=""
+                fi
 
-            # Echo the command without executing it
-            ceph orch host add "${hostname}" "$ip" "$labels"
+                # Echo the ceph command without executing it
+                ceph orch host add $hostname $ip $labels
+            fi
         fi
     fi
 done < /etc/hosts
--- Terraform/modules/servers/etchost.tpl
+++ Terraform/modules/servers/etchost.tpl
@@ -1,14 +1,14 @@
 127.0.0.1 localhost
-mon-0 ${join(" ", mon_0_private_ips)}
-mon-1 ${join(" ", mon_1_private_ips)}
-mon-2 ${join(" ", mon_2_private_ips)}
+${join(" ", mon_0_private_ips)} mon-0
+${join(" ", mon_1_private_ips)} mon-1
+${join(" ", mon_2_private_ips)} mon-2
 
-osd-0 ${join(" ", osd_0_private_ips)}
-osd-1 ${join(" ", osd_1_private_ips)}
-osd-2 ${join(" ", osd_2_private_ips)}
+${join(" ", osd_0_private_ips)} osd-0
+${join(" ", osd_1_private_ips)} osd-1
+${join(" ", osd_2_private_ips)} osd-2
 
-rgw-0 ${join(" ", rgw_0_private_ips)}
-rgw-1 ${join(" ", rgw_1_private_ips)}
+${join(" ", rgw_0_private_ips)} rgw-0
+${join(" ", rgw_1_private_ips)} rgw-1
 
 ff02::1 ip6-allnodes
 ff02::2 ip6-allrouters
